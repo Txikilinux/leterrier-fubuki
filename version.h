6740d67f36a1bf10dba5e028c1b764f0b6c3abd1
philippe.cadaugade <philippe.cadaugade@ryxeo.com>
2013-09-03 17:20:24 +0200

    Changement du numéro de version en 2.0.0

--- version.h
+++ version.h
@@ -24,16 +24,16 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VER_FILEVERSION             1,3,0,0
-#define VER_FILEVERSION_STR         "1.3.0\0"
+#define VER_FILEVERSION             2,0,0,0
+#define VER_FILEVERSION_STR         "2.0.0\0"
 
-#define VER_PRODUCTVERSION          1,3,0,0
-#define VER_PRODUCTVERSION_STR      "1.3.0\0"
+#define VER_PRODUCTVERSION          2,0,0,0
+#define VER_PRODUCTVERSION_STR      "2.0.0\0"
 
 #define VER_COMPANYNAME_STR         "RyXeo SARL"
 #define VER_FILEDESCRIPTION_STR     "AbulEdu LeTerrier -- Fubuki"
 #define VER_INTERNALNAME_STR        "leterrier-fubuki"
-#define VER_LEGALCOPYRIGHT_STR      "Copyright (c) 2012 RyXeo SARL"
+#define VER_LEGALCOPYRIGHT_STR      "Copyright (c) 2013 RyXeo SARL"
 #define VER_LEGALTRADEMARKS1_STR    "All Rights Reserved"
 #define VER_LEGALTRADEMARKS2_STR    "License GNU/GPL v3"
 #define VER_ORIGINALFILENAME_STR    "leterrier-fubuki.exe"
